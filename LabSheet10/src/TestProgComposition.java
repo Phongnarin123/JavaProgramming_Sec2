92f32a6ab1b8ca788823da21a8575b42586a99ee
Phongnarin123 <Phongnarinsatee@gmail.com>
2024-01-24 14:00:45 +0700

    LabSheet10

---
+++ LabSheet10/src/TestProgComposition.java
@@ -0,0 +1,10 @@
+
+public class TestProgComposition {
+
+	public static void main(String[] args) {
+		Personlinfo newStudent = new Personlinfo("William", "Jordan", 8, 24, 1963, 555238911);
+		System.out.println(newStudent);
+
+	}
+
+}
